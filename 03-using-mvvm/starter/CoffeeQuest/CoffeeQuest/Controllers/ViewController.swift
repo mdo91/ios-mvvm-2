f86a706ea00f4480daa040bbaef4790ac879f30d
mdo91 <hesoka_mxx@yahoo.com>
2021-01-08 21:29:59 +0300

    first commit

---
+++ 03-using-mvvm/starter/CoffeeQuest/CoffeeQuest/Controllers/ViewController.swift
@@ -0,0 +1,141 @@
+/// Copyright (c) 2020 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import MapKit
+import YelpAPI
+
+public class ViewController: UIViewController {
+  
+  // MARK: - Properties
+  var filter:Filter = Filter.identity()
+  private let client : BusinessSearchClient = YLPClient(apiKey: YelpAPIKey)
+  private let locationManager = CLLocationManager()
+  public let annotationFactory = AnnotationFactory()
+  
+  // MARK: - Outlets
+  @IBOutlet public var mapView: MKMapView! {
+    didSet {
+      mapView.showsUserLocation = true
+    }
+  }
+  
+  // MARK: - View Lifecycle
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    DispatchQueue.main.async { [weak self] in      self?.locationManager.requestWhenInUseAuthorization()
+    }
+  }
+  
+  // MARK: - Actions
+  @IBAction func businessFilterToggleChanged(_ sender: UISwitch) {
+    var businesses = filter.businesses
+    if sender.isOn{
+      filter = Filter.starRating(atLeast: 4.0)
+    }else{
+      filter = Filter.identity()
+    }
+    
+    filter.businesses = businesses
+    addAnnotations()
+    
+  }
+}
+
+// MARK: - MKMapViewDelegate
+extension ViewController: MKMapViewDelegate {
+  
+  public func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+    centerMap(on: userLocation.coordinate)
+  }
+  
+  private func centerMap(on coordinate: CLLocationCoordinate2D) {
+    let regionRadius: CLLocationDistance = 3000
+    let coordinateRegion = MKCoordinateRegion(center: coordinate,
+                                              latitudinalMeters: regionRadius,
+                                              longitudinalMeters: regionRadius)
+    mapView.setRegion(coordinateRegion, animated: true)
+    searchForBusinesses()
+  }
+  
+  private func searchForBusinesses() {
+    
+    client.search(with: mapView.userLocation.coordinate,
+                  term: "coffe",
+                  limit: 35,
+                  offset: 0) { [weak self] (businesses) in
+      
+      guard let self = self else{
+        return
+      }
+      
+      self.filter.businesses = businesses
+      
+      DispatchQueue.main.async { [self] in
+        self.addAnnotations()
+      }
+      
+      
+    } failure: { (error) in
+      print("Failure \(String(describing: error))")
+    }
+
+  }
+  
+  private func addAnnotations() {
+    mapView.removeAnnotations(mapView.annotations)
+    for business in filter {
+      
+       let viewModel = annotationFactory.createBusinessMapView(for: business)
+
+      mapView.addAnnotation(viewModel)
+    }
+  }
+  
+  public func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    guard  let viewModel = annotation as? BusinessMapViewModel else {
+      return nil
+    }
+    let identifier = "business"
+    let annotationView : MKAnnotationView
+    
+    if let existingView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier){
+      annotationView = existingView
+    }else{
+      annotationView = MKAnnotationView(annotation: viewModel, reuseIdentifier: identifier)
+    }
+    annotationView.image = viewModel.image
+    annotationView.canShowCallout = true
+    
+    return annotationView
+    
+  }
+}
